a583b0411020b296f13a0deb3cce38a862e8ed0e
ebcode2021 <ebcode2021@gmail.com>
2023-07-28 19:51:59 +0900

    [feat] footer 기본 세팅

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,16 @@
 const Footer = () => {
 	return (
-		<footer className="flex justify-between bg-red-400" style={{height: "10%"}}>
+		<footer className="flex justify-between items-center bg-gray-100 border-t-2 border-gray-200" style={{height: "10%", padding: "1%"}}>
 			<div>
-				footer
+				hi?
+			</div>
+			<div className="flex justify-between">
+				<div className="mr-4">
+					아이콘1
+				</div>
+				<div>
+					아이콘2
+				</div>
 			</div>
 		</footer>
 	);
