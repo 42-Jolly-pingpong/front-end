65a85824f61823a3f1e79b51f6f465e83e4306df
soong-e <yujelee@student.42seoul.kr>
2023-08-03 13:48:20 +0900

    feat: chat interface 수정

--- src/ts/interfaces/chat.model.tsx
+++ src/ts/interfaces/chat.model.tsx
@@ -1,8 +1,10 @@
+import { ChatType } from '../enum/chat-type.enum';
 import { User } from './userr.model';
 
 export interface Chat {
 	id: number;
 	user: User;
 	content: string;
-	sentTime: string; 
+	sentTime: string;
+	type: ChatType;
 }//임시s
