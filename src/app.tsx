e924ea17f9e0ed24b00885a04a446fe927e7e78b
ebcode2021 <ebcode2021@gmail.com>
2023-08-09 23:36:20 +0900

    [chore] version 변동, depth 변경에 따른 import 변경

--- src/app.tsx
+++ src/app.tsx
@@ -2,45 +2,44 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Sidebar from './components/sidebar/sidebar';
 import Home from './home';
-import {
-  RecoilRoot,
-  useRecoilValue,
-} from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 import { sidebarSelector } from './ts/state/sidebar-state';
 
-const Section = (props : {section: JSX.Element}) => {
-  const sidebarState = useRecoilValue(sidebarSelector)
+const Section = (props: { section: JSX.Element }) => {
+	const sidebarState = useRecoilValue(sidebarSelector);
 
-  return (
-    <div className="flex w-full h-full">
-      <div className="flex flex-col flex-grow justify-center items-center">
-      {props.section}
-      </div>
-      {sidebarState ? <Sidebar /> : null}
-    </div>
-  );
-} //임시
+	return (
+		<div className='flex w-full h-full'>
+			<div className='flex flex-col flex-grow justify-center items-center'>
+				{props.section}
+			</div>
+			{sidebarState ? <Sidebar /> : null}
+		</div>
+	);
+}; //임시
 
 function App() {
-
-  return (
-    <div>
-      <RecoilRoot>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Layout/>}>
-              <Route path="/" element={<Section section={<Home />} />} />
-              <Route path="/game" element={<h1>GAME</h1>} />
-              <Route path="/profile" element={<h1>GAME</h1>} />
-            </Route>
-            <Route path='/login' element={<h1>LOGIN</h1>} />
-            <Route path='/sign-up' element={<h1>SIGN-UP</h1>} />
-            <Route path='*' element={<h1>NOT FOUND</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </RecoilRoot>
-    </div>
-  );
+	return (
+		<div>
+			<RecoilRoot>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<Layout />}>
+							<Route
+								path='/'
+								element={<Section section={<Home />} />}
+							/>
+							<Route path='/game' element={<h1>GAME</h1>} />
+							<Route path='/profile' element={<h1>PROFILE</h1>} />
+						</Route>
+						<Route path='/login' element={<h1>LOGIN</h1>} />
+						<Route path='/sign-up' element={<h1>SIGN-UP</h1>} />
+						<Route path='*' element={<h1>NOT FOUND</h1>} />
+					</Routes>
+				</BrowserRouter>
+			</RecoilRoot>
+		</div>
+	);
 }
 
 export default App;
