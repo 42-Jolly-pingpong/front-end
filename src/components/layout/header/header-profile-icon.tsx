2be0810495a08b5374876e50f147e8d995e8f833
ebcode2021 <ebcode2021@gmail.com>
2023-08-09 23:35:12 +0900

    [feat] dropdown 기능 추가

--- src/components/layout/header/header-profile-icon.tsx
+++ src/components/layout/header/header-profile-icon.tsx
@@ -1,17 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 interface HeaderProfileIconProps {
 	src: string;
 }
 
 const HeaderProfileIcon: React.FC<HeaderProfileIconProps> = ({ src }) => {
+	const handleLogout = () => {
+		localStorage.removeItem('jwtToken');
+		console.log('hi');
+		return <Link to='/' />;
+	};
+
 	return (
-		<div className='avatar online layout-icon'>
-			<div className='rounded-full'>
-				<Link to='/profile'>
-					<img src={src} />
-				</Link>
-			</div>
+		<div className='dropdown dropdown-end'>
+			<label tabIndex={0} className='btn m-1'>
+				<button className='avatar online layout-icon' tabIndex={0}>
+					<img src={src} alt='Avatar' className='rounded-full' />
+				</button>
+			</label>
+			<ul
+				tabIndex={0}
+				className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'
+			>
+				<li>
+					<Link to='/profile'>SHOW PROFILE</Link>
+				</li>
+				<li>
+					<Link to='/'>LOGOUT</Link>
+					{/*<button onClick={handleLogout}> LOGOUT </button>*/}
+				</li>
+			</ul>
 		</div>
 	);
 };
