2d34cd98c31fff8ffc0267663cc09a8227a985a2
ebcode2021 <ebcode2021@gmail.com>
2023-08-09 23:34:57 +0900

    [feat] header-sign 컴포넌트 분리

---
+++ src/components/layout/header/header-sign.tsx
@@ -0,0 +1,10 @@
+const HeaderSign = () => {
+	return (
+		<div className='flex justify-between'>
+			<button className='btn'>SIGN IN</button>
+			<button className='btn'>SIGN UP</button>
+		</div>
+	);
+};
+
+export default HeaderSign;
