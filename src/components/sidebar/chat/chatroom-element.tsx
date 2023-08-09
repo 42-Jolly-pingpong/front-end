7a8c95d0bb31068e5275b6accb8bde381c320a9e
soong-e <yujelee@student.42seoul.kr>
2023-08-09 22:26:46 +0900

    feat: protected 타입 채팅방 표시

--- src/components/sidebar/chat/chatroom-element.tsx
+++ src/components/sidebar/chat/chatroom-element.tsx
@@ -1,3 +1,4 @@
+import { ChatroomType } from "../../../ts/enum/chatroom-type.enum";
 import { Chatroom } from "../../../ts/interfaces/chatroom.model copy";
 import { User } from "../../../ts/interfaces/userr.model";
 
@@ -8,13 +9,19 @@ export type ChatroomElementProps = {
 // props -> chat entity로 수정
 
 const ChatroomElement = (props: {chatroom: Chatroom}) => {
-	const user: User = props.chatroom.user;
+	const chatroom: Chatroom = props.chatroom;
+	const user: User = chatroom.user;
+
+	// const onClickChatroom = () => {
+		
+	// }
 
 	return (
-		<div className="flex items-center m-1 justify-between">
+		<div className="flex items-center m-1 justify-between" >
 			<div className="flex items-center justify-start">
 				<img src={user.avatar} className="rounded-full layout-icon w-12 h-12 mr-3"/>
-				{user.nickname}
+				{chatroom.title}
+				{chatroom.type === ChatroomType.PROTECTED ? null : "🔒"}
 			</div>
 		</div>
 	);
