c3ef18ba69449c547f27c2864d437562629e2ed1
soong-e <yujelee@student.42seoul.kr>
2023-08-11 18:15:26 +0900

    feat: status별 보여주는 component 분리
    status가 늘어나는 것을 대비해 분리함.

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,13 +1,26 @@
+import { SidebarStatus } from "../../ts/enum/sidebar-status.enum";
 import { sidebarSelector } from "../../ts/state/sidebar-state";
 import Chat from "./chat/chat";
 import Friend from "./friend/friend";
 import { useRecoilValue } from 'recoil';
 
-const Sidebar = () => {
+const SidebarContent = () => {
 	const state = useRecoilValue(sidebarSelector);
+	
+	switch (state){
+		case SidebarStatus.CHAT :
+			return (<Chat />);
+		case SidebarStatus.FRIEND :
+			return (<Friend />);
+	}
+	return null;
+}
 
-	return (<div className="w-80 h-full border border-solid border-black p-4 rounded-lg shadow-xl">
-		{state === 1 ? <Chat /> : <Friend />}
+const Sidebar = () => {
+
+	return (
+		<div className="w-80 h-full border border-solid border-black p-4 rounded-lg shadow-xl">
+			<SidebarContent />
 		</div>
 	);
 
