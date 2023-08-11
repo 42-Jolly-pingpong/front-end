b4e1509873fbbb4d38cea220d6303d9052a66ad0
soong-e <yujelee@student.42seoul.kr>
2023-08-11 18:18:19 +0900

    feat: 채팅룸 들어가고 나오도록 state변경

--- src/components/sidebar/chat/chatroom-element.tsx
+++ src/components/sidebar/chat/chatroom-element.tsx
@@ -1,18 +1,27 @@
-import { Chatroom } from "../../../ts/interfaces/chatroom.model copy";
+import { ChatStatus } from "../../../ts/enum/chat-status.enum";
+import { Chatroom } from "../../../ts/interfaces/chatroom.model";
+import { chatState, chatroomState } from "../../../ts/state/chat-state";
 import ChatroomInfo from "./chatroom-info";
+import { useRecoilState } from 'recoil';
 
 const ChatroomElement = (chatroom: Chatroom) => {
 	const owner = chatroom.user[0]; // 임시
-	// const onClickChatroom = () => {
-		
-	// }
+	const [currChatroom, setCurrChatroom] = useRecoilState(chatroomState);
+	const [currChat, setCurrChat] = useRecoilState(chatState);
+	
+	const onClickChatroom = () => {
+		setCurrChatroom(chatroom);
+		setCurrChat(ChatStatus.INCHAT);
+	}
 
 	return (
 		<div className="flex items-center m-1 justify-between" >
 			<div className="flex items-center justify-start">
-				<img src={owner.avatar} className="rounded-full layout-icon w-12 h-12 mr-3"/>
-				<div className="mr-2">
-					{chatroom.title}
+				<div className="flex items-center justify-start" onClick={onClickChatroom}>
+					<img src={owner.avatar} className="rounded-full layout-icon w-12 h-12 mr-3"/>
+					<div className="mr-2">
+						{chatroom.title}
+					</div>
 				</div>
 				<ChatroomInfo {... chatroom}/>
 			</div>
--- src/components/sidebar/inchat/chat-header.tsx
+++ src/components/sidebar/inchat/chat-header.tsx
@@ -1,13 +1,24 @@
-import { Chatroom } from "../../../ts/interfaces/chatroom.model copy";
+import { ChatStatus } from "../../../ts/enum/chat-status.enum";
+import { Chatroom } from "../../../ts/interfaces/chatroom.model";
+import { chatState, chatroomState } from "../../../ts/state/chat-state";
 import ChatroomInfo from "../chat/chatroom-info";
+import { useRecoilState } from 'recoil';
 
 const ChatHeader = (chatroom: Chatroom) => {
+	const [currChatroom, setCurrChatroom] = useRecoilState(chatroomState);
+	const [currChat, setCurrChat] = useRecoilState(chatState);
+
+	const onClickReturnButton = () => {
+		setCurrChatroom(null);
+		setCurrChat(ChatStatus.CHATLIST);
+	}
+
 	return (
-	<div className="flex">
-		<button>{"<"}</button>
-		<div className="ml-3 mr-2">{chatroom.title}</div>
-		<ChatroomInfo {...chatroom}/>
-	</div>
+		<div className="flex">
+			<button onClick={onClickReturnButton}>{"<"}</button>
+			<div className="ml-3 mr-2">{chatroom.title}</div>
+			<ChatroomInfo {...chatroom}/>
+		</div>
 	);
 }
 
