174e7ead64b1bc2ea872a41f275760e0e9cd7485
soong-e <yujelee@student.42seoul.kr>
2023-08-14 21:20:29 +0900

    feat: 친구 추가 모달 초안

--- src/components/sidebar/friend/friend-menu.tsx
+++ src/components/sidebar/friend/friend-menu.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import AddFriendModal from "./add-friend-modal";
+
 const FriendMenu = () => {
+	const [addFriendModal, setAddFriendModal] = useState(false);
+	const [blockedFriendModal, setBlockedFriendModal] = useState(false);
+
+	const onClickAddFriend = () => {
+		setAddFriendModal(true);
+	}
+
 	return (
 		<div className="flex justify-end">
-			<button className="btn btn-outline">+</button>
+			<button className="btn btn-outline" onClick={onClickAddFriend}>+</button>
+			<AddFriendModal state={addFriendModal} setState={setAddFriendModal}/>
 			<button className="btn btn-outline">블락</button>
 		</div>
 	);
---
+++ src/components/sidebar/friend/search-modal.tsx
@@ -0,0 +1,20 @@
+const SearchModal = (props : {setState: React.Dispatch<React.SetStateAction<boolean>>}) => {
+	//목록 가져오는 함수 받아오기.
+
+	return (
+		<div>
+			<div className="flex">
+				<input className="block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset"
+					placeholder="추가할 친구의 닉네임을 입력하세요.">
+				</input>
+				<button onClick={()=>{}}>🔎</button>
+				<button onClick={() => props.setState(false)}>x</button>
+			</div>
+			<div className="h-full overflow-y-auto">
+				{/*받아온 목록 map으로 띄우기*/}
+			</div>
+		</div>
+	);
+}
+
+export default SearchModal
