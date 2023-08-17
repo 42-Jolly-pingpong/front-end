e006308033671ba87868e83fb07e79c5f9f58d63
soong-e <yujelee@student.42seoul.kr>
2023-08-18 00:25:30 +0900

    feat: chat menu 생성

--- src/components/sidebar/chat/chat-menu.tsx
+++ src/components/sidebar/chat/chat-menu.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const ChatMenu = () => {
+	const context = useNavigate();
+
+	const aa = () => {
+		context('/create-chat');
+	}
+
 	return (
 		<div className="flex justify-end">
-			<button className="btn btn-outline">+</button>
+			<button className="btn btn-outline" onClick={aa}>+</button>
 		</div>
 	);
 }
