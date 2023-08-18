8e3c658fc3299657e78f4e55013ee719b4c9581b
ebcode2021 <ebcode2021@gmail.com>
2023-08-18 12:52:42 +0900

    [refactor] modal에서 기능 분리

---
+++ src/components/button/x-button.tsx
@@ -0,0 +1,9 @@
+const XButton = () => {
+	return (
+		<button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+			✕
+		</button>
+	);
+};
+
+export default XButton;
---
+++ src/components/modal/utils/backdrop.tsx
@@ -0,0 +1,9 @@
+const BackDrop = () => {
+	return (
+		<form method='dialog' className='modal-backdrop'>
+			<button>close</button>
+		</form>
+	);
+};
+
+export default BackDrop;
