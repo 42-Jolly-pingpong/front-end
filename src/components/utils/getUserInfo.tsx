62335d80a4e30786f6c12f0ce1250018e9d1952a
ebcode2021 <ebcode2021@gmail.com>
2023-08-18 12:45:49 +0900

    [feat] mockup user data 추가

---
+++ src/components/utils/getUserInfo.tsx
@@ -0,0 +1,9 @@
+import userData from '../../pages/profile/Mock-up/userData';
+import UserDTO from '../../pages/profile/userDto';
+
+const GetUserInfo = (): UserDTO | null => {
+	return null;
+	//return userData[1];
+};
+
+export default GetUserInfo;
