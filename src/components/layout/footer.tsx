ddf58d078ef94a721eccb812dd61d3daec587fe7
ebcode2021 <ebcode2021@gmail.com>
2023-08-18 12:43:26 +0900

    [feat] 로그인 여부에 따라 footer 달라짐

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,8 +2,12 @@ import { useRecoilState } from 'recoil';
 import { sidebarState } from '../../ts/state/sidebar-state';
 import { SidebarStatus } from '../../ts/enum/sidebar-state.enum';
 import WinRateModal from '../modal/win-rate-modal';
+import { useState } from 'react';
+
+import GetUserInfo from '../utils/getUserInfo';
 
 const Footer = () => {
+	const [user, setUser] = useState(GetUserInfo());
 	const [state, setState] = useRecoilState(sidebarState);
 
 	const onClickChat = () => {
@@ -27,23 +31,27 @@ const Footer = () => {
 			className='flex justify-between items-center bg-gray-100 border-t-2 border-gray-200'
 			style={{ height: '10%', padding: '1%' }}
 		>
-			<div>
-				<button
-					className='btn'
-					onClick={() => window.modal.showModal()}
-				>
-					Win-Rate
-				</button>
-				<WinRateModal />
-			</div>
-			<div className='flex justify-between'>
-				<button className='btn mr-4' onClick={onClickChat}>
-					Chat
-				</button>
-				<button className='btn mr-4' onClick={onClickFriend}>
-					Friend
-				</button>
-			</div>
+			{user && (
+				<>
+					<div>
+						<button
+							className='btn'
+							onClick={() => window.winRateModal.showModal()}
+						>
+							Win-Rate
+						</button>
+						<WinRateModal />
+					</div>
+					<div className='flex justify-between'>
+						<button className='btn mr-4' onClick={onClickChat}>
+							Chat
+						</button>
+						<button className='btn mr-4' onClick={onClickFriend}>
+							Friend
+						</button>
+					</div>
+				</>
+			)}
 		</footer>
 	);
 };
