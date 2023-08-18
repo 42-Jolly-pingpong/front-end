2e6928e0bcaeba0c86a8bcc01968a113a09ef3cb
ebcode2021 <ebcode2021@gmail.com>
2023-08-18 12:46:38 +0900

    [feat] 사용하는 modal 추가

--- src/types/index.d.tsx
+++ src/types/index.d.tsx
@@ -3,5 +3,13 @@ export {};
 declare global {
 	interface Window {
 		modal: any;
+		winRateModal: any;
+		gameWaitModal: any;
+		gameResultModal: any;
+		twoFactorAuthCheckModal: any;
+		twoFactorAuthModal: any;
+		emailAuthModal: any;
+		withdrawModal: any;
+		avatarChangeModal: any;
 	}
 }
