156398a5bfd0a5a0c31a913d7994a208749725e2
ebcode2021 <ebcode2021@gmail.com>
2023-08-18 12:53:29 +0900

    [feat] profile-badge

---
+++ src/components/profile/profile-badge.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserDTO from '../../pages/profile/userDto';
+
+const ProfileBadge: React.FC<UserDTO> = (props) => {
+	const { intra_id, status, is_leave } = props;
+
+	return (
+		<div className={`avatar layout-icon`} tabIndex={0}>
+			<img
+				src={props.avatar_path}
+				alt='Avatar'
+				className='rounded-full'
+			/>
+		</div>
+	);
+};
+
+export default ProfileBadge;
