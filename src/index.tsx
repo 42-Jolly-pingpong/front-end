0306a75ad2100da50197ce6a8bf17805fed90d65
soong-e <yujelee@student.42seoul.kr>
2023-09-04 20:52:03 +0900

    fix: 빠졌던 css파일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import App from 'app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'style/default.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
