0ed5f97920d16d332e91eec37be9ece5a33d6e97
get6 <ittae.com@gmail.com>
2023-09-08 21:05:45 +0900

    refactor: vite 환경에 맞게 변경 & 파일 삭제 #26

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import App from 'app';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'style/default.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.tsx'
+import './style/default.css'
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
-
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
