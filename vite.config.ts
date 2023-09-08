24fa74c3b59107b52e41c5a39fcac48a937692ca
get6 <ittae.com@gmail.com>
2023-09-08 21:07:06 +0900

    feat: alias를 사용하던 방식으로 변경 #26

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      ts: "/src/ts",
+      // '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
