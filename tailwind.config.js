d7ebfd5617977aafeb05e98369c2eb1d6f4a0331
get6 <ittae.com@gmail.com>
2023-09-08 20:08:59 +0900

    refactor: tailwind 실행 후 파일 생성 #26

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    //container: {
-    //  center: true,
-    //},
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
