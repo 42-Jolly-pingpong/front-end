8b26a829bde6b4b657d00eb9294f90abe01c1324
soong-e <yujelee@student.42seoul.kr>
2023-09-08 15:56:18 +0900

    feat: 뷰 수정

--- src/components/user-info/info-button.tsx
+++ src/components/user-info/info-button.tsx
@@ -4,7 +4,14 @@ type InfoButtonProps = {
 };
 
 const InfoButton = (props: InfoButtonProps) => {
-	return <button onClick={props.onClick}>{props.label}</button>;
+	return (
+		<button
+			onClick={props.onClick}
+			className='border border-solid border-black rounded-lg p-1 m-1'
+		>
+			{props.label}
+		</button>
+	);
 };
 
 export default InfoButton;
--- src/components/user-info/user-short-profile.tsx
+++ src/components/user-info/user-short-profile.tsx
@@ -8,7 +8,7 @@ const UserShortProfile = (props: { user: User }) => {
 			<img
 				src={user.avatarPath}
 				alt='Avatar'
-				className='rounded-full w-32 h-32'
+				className='rounded-full w-32 h-32 p-3 m-3'
 			/>
 			<h3>{user.nickname}</h3>
 		</div>
