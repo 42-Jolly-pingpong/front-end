a99ade407401cbf7e8e9edc030823c740c0df3e8
soong-e <yujelee@student.42seoul.kr>
2023-09-08 20:35:22 +0900

    feat: Avatar size 프롭스로 받기

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { User } from 'ts/interfaces/user.model';
 
-const Avatar = (props: { user: User }) => {
+const Avatar = (props: { user: User; size: number }) => {
 	const context = useNavigate();
 
 	const { user } = props;
@@ -14,9 +14,9 @@ const Avatar = (props: { user: User }) => {
 	return (
 		<button onClick={onClickAvatar}>
 			<div
-				className={`avatar ${
-					isOnline ? 'online' : 'offline'
-				} layout-icon w-16 h-16`}
+				className={`avatar ${isOnline ? 'online' : 'offline'} layout-icon w-${
+					props.size
+				} h-${props.size}`}
 			>
 				<img src={user.avatarPath} alt='Avatar' className='rounded-full' />
 			</div>
