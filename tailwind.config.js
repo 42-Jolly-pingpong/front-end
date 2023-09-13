d57efd49961f3d568d2cf2e7b3af23c3d3e8ef09
get6 <ittae.com@gmail.com>
2023-09-13 14:06:54 +0900

    chore: config에 flowbite 추가 #39

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('@tailwindcss/typography'), require("daisyui")
+    require('@tailwindcss/typography'),require("daisyui"), require('flowbite/plugin')
   ],
 }
