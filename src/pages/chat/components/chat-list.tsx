faa67c72888b545239ee5b80dea98df6fe8637c8
soong-e <yujelee@student.42seoul.kr>
2023-09-27 22:38:44 +0900

    feat: 챗목록에 로고 추가 #83

--- src/pages/chat/components/chat-list.tsx
+++ src/pages/chat/components/chat-list.tsx
@@ -1,5 +1,13 @@
+import Logo from 'components/logo/logo';
+
 const ChatList = () => {
-	return <div className='h-screen w-60 border-r'></div>;
+	return (
+		<div className='h-screen w-60 border-r'>
+			<div className='border-b'>
+				<Logo />
+			</div>
+		</div>
+	);
 };
 
 export default ChatList;
