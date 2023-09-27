bd26701cf72e57ff37317676f325e4f1bf33ad23
soong-e <yujelee@student.42seoul.kr>
2023-09-28 00:14:58 +0900

    feat: theme 추가 #83

--- src/pages/chat/components/chat-list.tsx
+++ src/pages/chat/components/chat-list.tsx
@@ -1,10 +1,20 @@
 import Logo from 'components/logo/logo';
+import { Flowbite, Sidebar } from 'flowbite-react';
+import Channels from 'pages/chat/components/channels';
+import { sidebarTheme } from 'pages/chat/components/side-bar-theme';
 
 const ChatList = () => {
 	return (
 		<div className='h-screen w-60 border-r'>
 			<div className='border-b'>
 				<Logo />
+				<Flowbite theme={{ theme: sidebarTheme }}>
+					<Sidebar className='w-full'>
+						<Sidebar.ItemGroup>
+							<Channels />
+						</Sidebar.ItemGroup>
+					</Sidebar>
+				</Flowbite>
 			</div>
 		</div>
 	);
---
+++ src/pages/chat/components/side-bar-theme.ts
@@ -0,0 +1,52 @@
+import { CustomFlowbiteTheme } from 'flowbite-react';
+
+export const sidebarTheme: CustomFlowbiteTheme = {
+	sidebar: {
+		root: {
+			base: 'h-full',
+			inner: 'h-full overflow-y-auto overflow-x-hidden rounded',
+		},
+		collapse: {
+			button:
+				'group flex w-full items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700',
+			icon: {
+				base: 'h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
+				open: {
+					off: '',
+					on: 'text-gray-900',
+				},
+			},
+			label: {
+				base: 'ml-3 flex-1 whitespace-nowrap text-left',
+				icon: {
+					base: 'h-6 w-6 transition ease-in-out delay-0',
+					open: {
+						on: 'rotate-180',
+						off: '',
+					},
+				},
+			},
+			list: 'space-y-2 py-2',
+		},
+		item: {
+			base: 'flex items-center justify-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700',
+			active: 'bg-gray-100 dark:bg-gray-700',
+			collapsed: {
+				insideCollapse: 'group w-full pl-8 transition duration-75',
+				noIcon: 'font-bold',
+			},
+			content: {
+				base: 'px-3 flex-1 whitespace-nowrap',
+			},
+			icon: {
+				base: 'h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
+				active: 'text-gray-700 dark:text-gray-100',
+			},
+			label: '',
+			listItem: '',
+		},
+		items: '',
+		itemGroup:
+			'mt-4 space-y-2 border-t border-gray-200 pt-4 first:mt-0 first:border-t-0 first:pt-0 dark:border-gray-700',
+	},
+};
