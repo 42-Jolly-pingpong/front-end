ddf114291b89564f8bdc936e59e76b5b15408346
ebcode2021 <ebcode2021@gmail.com>
2023-09-27 22:13:55 +0900

    sign up page 추가

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -1,13 +1,48 @@
 import { getJsonValueByKey } from 'components/utils/cookieUtils';
 import { Button, Card, Label, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
 	const cookies = getJsonValueByKey('user-data');
+	const [nickname, setNickname] = useState('');
+	const navigate = useNavigate();
+
+	const handleNickname = (e) => {
+		setNickname(e.target.value);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const formData = {
+			intraId: cookies.intraId,
+			email: cookies.email,
+			nickname,
+		};
+		try {
+			const response = await fetch(`http://localhost:3000/auth/signup`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(formData),
+			});
+
+			if (response.ok) {
+				navigate('/');
+			} else {
+				console.log('회원가입 실패 냠냠');
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
 	return (
 		<div className='flex flex-col justify-center items-center h-4/5'>
 			<Card className='flex w-1/4'>
-				<form className='flex flex-col gap-3'>
+				<form className='flex flex-col gap-3' onSubmit={handleSubmit}>
 					<div className='text-xl'>Sign up to Jolly Ping Pong</div>
 					<Label htmlFor='email' value='Your email' className='my-2' />
 					<TextInput
@@ -17,7 +52,12 @@ const SignUp = () => {
 						readOnly
 					/>
 					<Label htmlFor='nickname' value='nickname' className='my-2' />
-					<TextInput id='nickname' type='text' required />
+					<TextInput
+						id='nickname'
+						type='text'
+						required
+						onChange={handleNickname}
+					/>
 					<Button
 						type='submit'
 						className='bg-yellow-300 border border-transparent enabled:hover:bg-yellow-300 mt-7'
