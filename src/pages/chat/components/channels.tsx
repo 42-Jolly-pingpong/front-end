ca969b5dcc2692cc8fee7539e4c44a020da5f086
soong-e <yujelee@student.42seoul.kr>
2023-09-28 00:14:31 +0900

    feat: 채널 컴포넌트 추가 #83

---
+++ src/pages/chat/components/channels.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from 'flowbite-react';
+
+const Channels = () => {
+	return (
+		<Sidebar.Collapse label='채널'>
+			<Sidebar.Item href='#'>Products</Sidebar.Item>
+		</Sidebar.Collapse>
+	);
+};
+
+export default Channels;
