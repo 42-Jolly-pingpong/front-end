b448f9d8abb8527884eaf1cfea9b21d5307446a9
soong-e <yujelee@student.42seoul.kr>
2023-09-29 01:05:07 +0900

    feat: 모달 크기 지정 #84

--- src/pages/chat/components/modal/channel-modal.tsx
+++ src/pages/chat/components/modal/channel-modal.tsx
@@ -1,10 +1,10 @@
 import { Modal } from 'flowbite-react';
 import { CreateChannelModal } from 'pages/chat/components/modal/create-channel-modal';
 import AddChannelModal from 'pages/chat/components/modal/add-channel-modal';
-import SearchChannelModal from 'pages/chat/components/modal/search-channel-modal';
 import { useRecoilState } from 'recoil';
 import { ChatModalStatus } from 'ts/enums/chat-modal-status.enum';
 import { chatModalState } from 'ts/states/chat-modal-state';
+import SearchChannelModal from 'pages/chat/components/modal/search-channel-modal';
 
 const ChannelModal = () => {
 	const [modalStatus, setModalStatus] = useRecoilState(chatModalState);
@@ -25,7 +25,11 @@ const ChannelModal = () => {
 	};
 
 	return (
-		<Modal show={modalStatus !== ChatModalStatus.CLOSE} onClose={onCloseModal}>
+		<Modal
+			show={modalStatus !== ChatModalStatus.CLOSE}
+			onClose={onCloseModal}
+			className='max-h-screen'
+		>
 			{modalContent()}
 		</Modal>
 	);
