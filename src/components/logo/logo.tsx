0a5f18e49f49e31168eb092e42c4c6dd07a9e20f
soong-e <yujelee@student.42seoul.kr>
2023-09-28 14:24:30 +0900

    fix: 로고 div -> button으로 변환

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -8,10 +8,10 @@ const Logo = () => {
 	};
 
 	return (
-		<div className='flex items-center' onClick={onClickLogo}>
+		<button className='flex items-center' onClick={onClickLogo}>
 			<img src='/images/logo2.jpeg' className='w-6 h-6 m-3'></img>
 			<div className='font-semibold'>죠리핑퐁</div>
-		</div>
+		</button>
 	);
 };
 
