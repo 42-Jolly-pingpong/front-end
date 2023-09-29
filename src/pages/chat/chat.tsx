e694f38d3fe1e85698aaf0733e3aa18a61bdd501
soong-e <yujelee@student.42seoul.kr>
2023-09-29 14:43:39 +0900

    refactor: 컴포넌트 뎁스 변경

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,14 +1,15 @@
+import ChatContent from 'pages/chat/chat-content';
 import ChatList from 'pages/chat/components/chat-list';
-import ChannelModal from 'pages/chat/components/modal/channel-modal';
 
 const Chat = () => {
 	return (
-		<>
+		<div className='flex'>
 			<div className='flex max-h-screen max-w-screen'>
 				<ChatList />
+				<ChatContent />
+				<ChannelModal />
 			</div>
-			<ChannelModal />
-		</>
+		</div>
 	);
 };
 
