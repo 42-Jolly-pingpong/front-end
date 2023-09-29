6570bc5b70f9323dd55d68fd260cd67b4c3db00a
soong-e <yujelee@student.42seoul.kr>
2023-09-29 14:45:14 +0900

    feat: 채널 아이템 눌렀을 시 state 변경하도록 수정 #95

--- src/pages/chat/components/channel-item.tsx
+++ src/pages/chat/components/channel-item.tsx
@@ -1,10 +1,18 @@
 import { Sidebar } from 'flowbite-react';
 import { ChatRoom } from 'ts/interfaces/chat-room.model';
 import ChannelIcon from 'pages/chat/channel-icon';
+import { useSetRecoilState } from 'recoil';
+import { chatState } from 'ts/states/chat-state';
 
 const ChannelItem = (props: { channel: ChatRoom }) => {
+	const setChat = useSetRecoilState(chatState);
+
+	const onClickItem = () => {
+		setChat(props.channel);
+	};
+
 	return (
-		<Sidebar.Item>
+		<Sidebar.Item onClick={onClickItem}>
 			<div className='flex items-center'>
 				<div className='mr-1'>
 					<ChannelIcon roomType={props.channel.roomType} />
