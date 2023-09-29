702dddf78deeac742e633163336b955a637c7912
soong-e <yujelee@student.42seoul.kr>
2023-09-29 13:24:40 +0900

    feat: 채널 아이콘 컴포넌트 분리 #95

--- src/pages/chat/components/channel-item.tsx
+++ src/pages/chat/components/channel-item.tsx
@@ -1,25 +1,14 @@
 import { Sidebar } from 'flowbite-react';
-import { ChatRoomType } from 'ts/enums/chat-room-type.enum';
 import { ChatRoom } from 'ts/interfaces/chat-room.model';
-import { BiHash, BiLock, BiLockOpen } from 'react-icons/bi';
+import ChannelIcon from 'pages/chat/channel-icon';
 
 const ChannelItem = (props: { channel: ChatRoom }) => {
-	const channelIcon = () => {
-		switch (props.channel.roomType) {
-			case ChatRoomType.PUBLIC:
-				return <BiHash />;
-
-			case ChatRoomType.PROTECTED:
-				return <BiLockOpen />;
-
-			case ChatRoomType.PRIVATE:
-				return <BiLock />;
-		}
-	};
 	return (
 		<Sidebar.Item>
 			<div className='flex items-center'>
-				<div className='mr-1'>{channelIcon()}</div>
+				<div className='mr-1'>
+					<ChannelIcon roomType={props.channel.roomType} />
+				</div>
 				{props.channel.roomName}
 			</div>
 		</Sidebar.Item>
