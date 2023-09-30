1bb90eb5ae9700d784e3dae4bb7011a9d17ed548
soong-e <yujelee@student.42seoul.kr>
2023-09-30 21:38:04 +0900

    feat: chat-field 스크롤 제일 밑으로 내림 #82

--- src/pages/chat/components/chat-field.tsx
+++ src/pages/chat/components/chat-field.tsx
@@ -1,14 +1,30 @@
+import DmHeader from 'pages/chat/components/dm-header';
+import { useEffect, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
+import { ChatRoomType } from 'ts/enums/chat-room-type.enum';
+import { Dm } from 'ts/interfaces/dm.model';
 import { chatState } from 'ts/states/chat-state';
 
 const ChatField = () => {
+	const scrollRef = useRef<HTMLDivElement>(null);
 	const chat = useRecoilValue(chatState);
+
+	useEffect(() => {
+		if (scrollRef.current) {
+			scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+		}
+	}, [scrollRef]);
+
 	if (chat === null) {
 		return <></>;
 	}
-	const type = chat?.roomType;
+	const isDm = chat.roomType === ChatRoomType.DM;
 
-	return <div className='overflow-y-auto'></div>;
+	return (
+		<div ref={scrollRef} className='chat-field overflow-y-auto justify-end'>
+			{isDm && <DmHeader mate={(chat as Dm).chatMate} />}
+		</div>
+	);
 };
 
 export default ChatField;
