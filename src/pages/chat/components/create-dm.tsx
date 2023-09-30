518a151a1a3f2ba661bc203b112614fdffa248be
soong-e <yujelee@student.42seoul.kr>
2023-09-30 18:24:49 +0900

    feat: 대화상대 추가에 클릭 이벤트 추가 #85

--- src/pages/chat/components/create-dm.tsx
+++ src/pages/chat/components/create-dm.tsx
@@ -1,9 +1,17 @@
 import { Sidebar } from 'flowbite-react';
 import { BiPlus } from 'react-icons/bi';
+import { useSetRecoilState } from 'recoil';
+import { chatHeaderState } from 'ts/states/chat-header-state';
 
 const CreateDm = () => {
+	const setChatHeaderState = useSetRecoilState(chatHeaderState);
+
+	const onClickCreate = () => {
+		setChatHeaderState(false);
+	};
+
 	return (
-		<Sidebar.Item>
+		<Sidebar.Item onClick={onClickCreate}>
 			<div className='flex items-center'>
 				<BiPlus className='mr-1' />
 				<div>대화상대 추가</div>
