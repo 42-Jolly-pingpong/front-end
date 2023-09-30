027ebc2d2a2a8a59d9bee4b1068312ec45612fbe
soong-e <yujelee@student.42seoul.kr>
2023-10-01 01:05:05 +0900

    feat: dm검색결과 온클릭 이벤트 추가 #85

--- src/pages/chat/components/dm-searched-item.tsx
+++ src/pages/chat/components/dm-searched-item.tsx
@@ -1,13 +1,24 @@
 import { Avatar } from 'flowbite-react';
+import changeChat from 'hooks/change-chat';
+import { dmList } from 'pages/chat/mock';
 import { User } from 'ts/interfaces/user.model';
 
 const DmSearchedItem = (props: { friend: User; isTheLast: boolean }) => {
 	const { friend } = props;
+	const setChat = changeChat();
 	const margin = props.isTheLast ? '' : 'mb-1';
+
+	const onClickFriend = () => {
+		const dm = dmList[0]; //temp
+		setChat(dm);
+	};
 
 	return (
 		<div className={`w-full ${margin}`}>
-			<button className='flex px-3 py-1 w-full hover:bg-gray-200 text-left'>
+			<button
+				className='flex px-3 py-1 w-full hover:bg-gray-200 text-left'
+				onClick={onClickFriend}
+			>
 				<Avatar img={friend.avatarPath} size='xs' className='mr-2' />
 				{friend.nickname}
 			</button>
