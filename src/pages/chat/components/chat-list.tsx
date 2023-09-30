9716a6792146fd6cd9d587bca02daf17e8b2ba8a
soong-e <yujelee@student.42seoul.kr>
2023-09-30 20:49:17 +0900

    fix: sidebar아래 보더 제거

--- src/pages/chat/components/chat-list.tsx
+++ src/pages/chat/components/chat-list.tsx
@@ -7,20 +7,18 @@ import { sidebarTheme } from 'pages/chat/themes/side-bar-theme';
 const ChatList = () => {
 	return (
 		<div className='h-screen w-60 border-r'>
-			<div className='border-b'>
-				<Logo />
-				<div className='sidebar'>
-					<Flowbite theme={{ theme: sidebarTheme }}>
-						<Sidebar className='w-full'>
-							<Sidebar.ItemGroup>
-								<Channels />
-							</Sidebar.ItemGroup>
-							<Sidebar.ItemGroup>
-								<Dms />
-							</Sidebar.ItemGroup>
-						</Sidebar>
-					</Flowbite>
-				</div>
+			<Logo />
+			<div className='sidebar'>
+				<Flowbite theme={{ theme: sidebarTheme }}>
+					<Sidebar className='w-full'>
+						<Sidebar.ItemGroup>
+							<Channels />
+						</Sidebar.ItemGroup>
+						<Sidebar.ItemGroup>
+							<Dms />
+						</Sidebar.ItemGroup>
+					</Sidebar>
+				</Flowbite>
 			</div>
 		</div>
 	);
