95bade5bee888773a5c9f3ab57da17f6ed8de89f
soong-e <yujelee@student.42seoul.kr>
2023-10-03 21:46:54 +0900

    feat: 남은 숫자 나오게 표시 #100

--- src/pages/chat/components/modal/set-chat-room-name.tsx
+++ src/pages/chat/components/modal/set-chat-room-name.tsx
@@ -1,6 +1,7 @@
 import { Button, TextInput } from 'flowbite-react';
 import { CreateChatRoom } from 'ts/interfaces/create-chat-room.model';
 import { BiHash } from 'react-icons/bi';
+import { useState } from 'react';
 
 interface FormElements extends HTMLFormControlsCollection {
 	roomName: HTMLInputElement;
@@ -16,6 +17,7 @@ const SetChatRoomName = (props: {
 	setChatRoomInfo: React.Dispatch<React.SetStateAction<CreateChatRoom>>;
 }) => {
 	const { setPhase, setChatRoomInfo } = props;
+	const [nameLength, setNameLength] = useState<number>(80);
 
 	const onSubmitName = (e: React.FormEvent<FormElement>) => {
 		e.preventDefault();
@@ -29,26 +31,41 @@ const SetChatRoomName = (props: {
 		setPhase(2);
 	};
 
+	const byteLeft = () => {
+		return <div>{nameLength}</div>;
+	};
+
+	const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNameLength(80 - e.target.value.length);
+	};
+
 	return (
-		<form onSubmit={onSubmitName}>
-			<h3>이름</h3>
-			<TextInput
-				id='roomName'
-				icon={BiHash}
-				className='mt-3 mb-3'
-				required
-				defaultValue={props.chatRoomInfo.roomName}
-				placeholder='예: 게임 고수만'
-			/>
-			<div className='ml-1 text-xs font-normal text-gray-500'>
-				채널에서는 특정 주제에 대한 대화가 이루어집니다. 찾고 이해하기 쉬운
-				이름을 사용하세요.
+		<div className='relative'>
+			<form onSubmit={onSubmitName}>
+				<h3>이름</h3>
+				<TextInput
+					id='roomName'
+					icon={BiHash}
+					className='mt-3 mb-3'
+					required
+					defaultValue={props.chatRoomInfo.roomName}
+					placeholder='예: 게임 고수만'
+					maxLength={80}
+					onChange={onChangeName}
+				/>
+				<div className='ml-1 text-xs font-normal text-gray-500'>
+					채널에서는 특정 주제에 대한 대화가 이루어집니다. 찾고 이해하기 쉬운
+					이름을 사용하세요.
+				</div>
+				<div className='ml-1 flex justify-between items-center mt-5'>
+					<div className='text-sm font-light text-gray-900'>1/2단계</div>
+					<Button type='submit'>다음</Button>
+				</div>
+			</form>
+			<div className='absolute top-12 right-4 text-sm font-normal text-gray-500'>
+				{nameLength}
 			</div>
-			<div className='ml-1 flex justify-between items-center mt-5'>
-				<div className='text-sm font-light text-gray-900'>1/2단계</div>
-				<Button type='submit'>다음</Button>
-			</div>
-		</form>
+		</div>
 	);
 };
 
