25c45f40fadb6427777671bb2f82682e98217021
soong-e <yujelee@student.42seoul.kr>
2023-10-03 18:32:32 +0900

    fix: 채널 누르면 사이드바 닫히게 수정

--- src/hooks/change-chat.tsx
+++ src/hooks/change-chat.tsx
@@ -1,16 +1,24 @@
 import { useSetRecoilState } from 'recoil';
+import { ChatSidebarStatus } from 'ts/enums/chat-sidebar-status.enum';
 import { ChatRoom } from 'ts/interfaces/chat-room.model';
 import { Dm } from 'ts/interfaces/dm.model';
 import { chatHeaderState } from 'ts/states/chat-header-state';
+import { chatSidebarState } from 'ts/states/chat-sidebar-state';
 import { chatState } from 'ts/states/chat-state';
 
 const changeChat = () => {
 	const setChatState = useSetRecoilState(chatState);
 	const setChatHeaderState = useSetRecoilState(chatHeaderState);
+	const setSidebarState = useSetRecoilState(chatSidebarState);
 
 	const setChat = (chat: ChatRoom | Dm | null) => {
 		setChatHeaderState(true);
 		setChatState(chat);
+		setSidebarState({
+			status: ChatSidebarStatus.CLOSE,
+			chat: null,
+			profile: null,
+		});
 	};
 
 	return setChat;
