56702c429acf40e1c1da7e36af26b6896583e9e4
ebcode2021 <ebcode2021@gmail.com>
2023-10-03 16:56:50 +0900

    section 파일 분리

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -1,7 +1,30 @@
-const Section = () => {
+import { Sidebar } from 'flowbite-react';
+import { useRecoilValue } from 'recoil';
+import { sidebarSelector } from 'ts/states/sidebar-state';
+
+const Section = (props: { section: JSX.Element }) => {
+	const sidebarState = useRecoilValue(sidebarSelector);
+
 	return (
-		
+		<div>
+			<div>{props.section}</div>
+			{sidebarState ? <Sidebar /> : null}
+		</div>
 	);
-}
+};
 
 export default Section;
+
+//const Section = (props: { section: JSX.Element }) => {
+//	const sidebarState = useRecoilValue(sidebarSelector);
+
+//	return (
+//		<div className='flex w-full h-full'>
+//			<div className='flex flex-col flex-grow justify-center items-center'>
+//				{props.section}
+//			</div>
+//			{sidebarState ? <Sidebar /> : null}
+//			<Snackbar />
+//		</div>
+//	);
+//}; //임시
