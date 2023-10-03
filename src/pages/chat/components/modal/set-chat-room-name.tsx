b5145633e7f14c922e83717dad2eda695ba6ea11
soong-e <yujelee@student.42seoul.kr>
2023-10-03 21:22:04 +0900

    feat: 채널 이름 모달 구현 #100

--- src/pages/chat/components/modal/set-chat-room-name.tsx
+++ src/pages/chat/components/modal/set-chat-room-name.tsx
@@ -1,5 +1,6 @@
 import { Button, TextInput } from 'flowbite-react';
 import { CreateChatRoom } from 'ts/interfaces/create-chat-room.model';
+import { BiHash } from 'react-icons/bi';
 
 interface FormElements extends HTMLFormControlsCollection {
 	roomName: HTMLInputElement;
@@ -30,14 +31,21 @@ const SetChatRoomName = (props: {
 
 	return (
 		<form onSubmit={onSubmitName}>
-			<h3>채널 이름</h3>
+			<h3>이름</h3>
 			<TextInput
 				id='roomName'
+				icon={BiHash}
 				className='mt-3 mb-3'
 				required
 				defaultValue={props.chatRoomInfo.roomName}
+				placeholder='예: 게임 고수만'
 			/>
-			<div className='flex justify-end'>
+			<div className='ml-1 text-xs font-normal text-gray-500'>
+				채널에서는 특정 주제에 대한 대화가 이루어집니다. 찾고 이해하기 쉬운
+				이름을 사용하세요.
+			</div>
+			<div className='ml-1 flex justify-between items-center mt-5'>
+				<div className='text-sm font-light text-gray-900'>1/2단계</div>
 				<Button type='submit'>다음</Button>
 			</div>
 		</form>
