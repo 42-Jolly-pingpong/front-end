c2efda660e7ecea82ff20e846252d06ceae3d255
soong-e <yujelee@student.42seoul.kr>
2023-10-03 21:15:46 +0900

    feat: 모달 종류 고르는 곳 드롭다운으로 수정 #100

--- src/pages/chat/components/list/create-channel.tsx
+++ src/pages/chat/components/list/create-channel.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from 'flowbite-react';
+import { Dropdown, Sidebar } from 'flowbite-react';
 import { BiPlus } from 'react-icons/bi';
 import { useSetRecoilState } from 'recoil';
 import { ChatModalStatus } from 'ts/enums/chat-modal-status.enum';
@@ -7,17 +7,34 @@ import { chatModalState } from 'ts/states/chat-modal-state';
 const CreateChannel = () => {
 	const setModalStatus = useSetRecoilState(chatModalState);
 
-	const onClickCreateChannel = () => {
-		setModalStatus(ChatModalStatus.OPEN);
+	const onClickCreate = () => {
+		setModalStatus(ChatModalStatus.CREATE);
+	};
+
+	const onClickSearch = () => {
+		setModalStatus(ChatModalStatus.SEARCH);
+	};
+
+	const addChannalButton = () => {
+		return (
+			<Sidebar.Item>
+				<div className='flex items-center'>
+					<BiPlus className='mr-2' size='20' />
+					<div className='text-base font-medium'>채널 추가</div>
+				</div>
+			</Sidebar.Item>
+		);
 	};
 
 	return (
-		<Sidebar.Item onClick={onClickCreateChannel}>
-			<div className='flex items-center'>
-				<BiPlus className='mr-2' size='20' />
-				<div className='text-base font-medium'>채널 추가</div>
-			</div>
-		</Sidebar.Item>
+		<Dropdown
+			label='채널 추가하기'
+			dismissOnClick={false}
+			renderTrigger={() => addChannalButton()}
+		>
+			<Dropdown.Item onClick={onClickCreate}>새 채널 생성</Dropdown.Item>
+			<Dropdown.Item onClick={onClickSearch}>채널 탐색</Dropdown.Item>
+		</Dropdown>
 	);
 };
 
--- src/pages/chat/components/modal/channel-modal.tsx
+++ src/pages/chat/components/modal/channel-modal.tsx
@@ -1,6 +1,5 @@
 import { Modal } from 'flowbite-react';
 import { CreateChannelModal } from 'pages/chat/components/modal/create-channel-modal';
-import AddChannelModal from 'pages/chat/components/modal/add-channel-modal';
 import { useRecoilState } from 'recoil';
 import { ChatModalStatus } from 'ts/enums/chat-modal-status.enum';
 import { chatModalState } from 'ts/states/chat-modal-state';
@@ -15,8 +14,6 @@ const ChannelModal = () => {
 
 	const modalContent = () => {
 		switch (modalStatus) {
-			case ChatModalStatus.OPEN:
-				return <AddChannelModal />;
 			case ChatModalStatus.CREATE:
 				return <CreateChannelModal />;
 			case ChatModalStatus.SEARCH:
