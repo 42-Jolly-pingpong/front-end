f8a5d92f6a72dfa44203a3b9929b7e8bbad86e46
soong-e <yujelee@student.42seoul.kr>
2023-10-04 18:25:00 +0900

    feat: useFetch hook 생성

---
+++ src/hooks/use-fetch.tsx
@@ -0,0 +1,26 @@
+const useFetch = () => {
+	//토큰 꺼내오기
+
+	const baseUrl = 'http://localhost:3000';
+	/**
+	 * api 보내기
+	 * @param method
+	 * @param url start with '/'
+	 * @param body
+	 */
+	const sendMsg = (method: string, url: string, body: any = null) => {
+		if (method === 'get') {
+			return fetch(baseUrl + url, {
+				method,
+			});
+		}
+		return fetch(baseUrl + url, {
+			method,
+			body: JSON.stringify(body),
+		});
+	};
+
+	return sendMsg;
+};
+
+export default useFetch;
