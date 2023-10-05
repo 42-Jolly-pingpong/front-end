83ce4d0c244864bfca2dbb0fcc3408d296f527e2
soong-e <yujelee@student.42seoul.kr>
2023-10-05 16:23:15 +0900

    feat: 새로운 채널 들어가기 구현

--- src/pages/chat/components/modal/search-channel-item.tsx
+++ src/pages/chat/components/modal/search-channel-item.tsx
@@ -6,14 +6,27 @@ import { useState } from 'react';
 import userData from 'ts/mock/user-data';
 import { ChatParticipantRole } from 'ts/enums/chat-participants-role.enum';
 import { BsDot } from 'react-icons/bs';
+import useChangeChat from 'hooks/use-change-chat';
+import { useSetRecoilState } from 'recoil';
+import { chatListState } from 'ts/states/chat-list.state';
+import useFetch from 'hooks/use-fetch';
+import { chatModalState } from 'ts/states/chat-modal-state';
+import { ChatModalStatus } from 'ts/enums/chat-modal-status.enum';
 
 export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 	const [isHovered, setIsHovered] = useState(false);
+	const setChat = useChangeChat();
+	const setChannelList = useSetRecoilState(chatListState);
+	const setModalStatus = useSetRecoilState(chatModalState);
+	const sendApi = useFetch();
 	const owner = props.channel.participants.find(
 		(participant) => participant.role === ChatParticipantRole.OWNER
 	);
-
 	const user = userData[0]; //temp
+	const isUserInChannel =
+		props.channel.participants.filter(
+			(participant) => participant.user.id == user.id
+		).length !== 0;
 
 	const onMouseEnter = () => {
 		setIsHovered(true);
@@ -33,16 +46,8 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 		}
 	};
 
-	const isUserInChannel = () => {
-		const result = props.channel.participants.filter(
-			(participant) => participant.user.id == user.id
-		);
-
-		return result.length !== 0;
-	};
-
 	const buttonOnChannel = () => {
-		if (isUserInChannel()) {
+		if (isUserInChannel) {
 			return (
 				<div className='h-8 border border-gray-200 focus:outline-none text-gray-800 bg-white hover:bg-gray-300 focus:ring-4 focus:ring-cyan-300 text-xs font-medium rounded-lg px-3 py-2'>
 					이동
@@ -65,15 +70,42 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 		);
 	};
 
+	const onClickItem = () => {
+		if (isUserInChannel) {
+			setChat(props.channel);
+			setModalStatus(ChatModalStatus.CLOSE);
+			return;
+		}
+		(async () => {
+			//temp 비번있으면 또 해쉬해서 쳐보내야함..
+			await sendApi('POST', `/chat-rooms/${props.channel.id}`)
+				.then((res) => res.json())
+				.then((data: ChatRoom) => {
+					setChannelList((pre) => {
+						return {
+							...pre,
+							channelList: [...pre.channelList, data],
+						};
+					});
+					setChat(data);
+				});
+			setModalStatus(ChatModalStatus.CLOSE);
+		})();
+	};
+
 	return (
-		<ListGroup.Item onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+		<ListGroup.Item
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={onMouseLeave}
+			onClick={onClickItem}
+		>
 			<div className='flex flex-col items-start'>
 				<div className='flex items-center text-sm font-bold text-gray-900'>
 					<div className='mr-1'>{channelIcon()}</div>
 					{props.channel.roomName}
 				</div>
 				<div className='flex items-center text-xm text-gray-500 font-light'>
-					{isUserInChannel() && badge()}
+					{isUserInChannel && badge()}
 					{props.channel.currentPeople}명의 멤버
 					<BsDot />
 					채널 소요주 : {owner?.user.intraId}
