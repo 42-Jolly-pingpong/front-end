e39749cd93c6507a14ed75199089fd4d9162bd5e
soong-e <yujelee@student.42seoul.kr>
2023-10-05 13:02:50 +0900

    feat: 디엠 추가

--- src/pages/chat/components/header/dm-searched-item.tsx
+++ src/pages/chat/components/header/dm-searched-item.tsx
@@ -1,20 +1,46 @@
 import { Avatar } from 'flowbite-react';
 import useChangeChat from 'hooks/use-change-chat';
-import { dmList } from 'pages/chat/mock';
+import useFetch from 'hooks/use-fetch';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { Dm } from 'ts/interfaces/dm.model';
 import { User } from 'ts/interfaces/user.model';
+import { chatListSelector, chatListState } from 'ts/states/chat-list.state';
 
 const DmSearchedItem = (props: { friend: User; isTheLast: boolean }) => {
 	const { friend } = props;
 	const setChat = useChangeChat();
-	const margin = props.isTheLast ? '' : 'mb-1';
+	const setDmList = useSetRecoilState(chatListState);
+	const dmList = useRecoilValue(chatListSelector).dmList;
+	const sendApi = useFetch();
+
+	const createNewDm = async () => {
+		await sendApi('POST', '/chat-rooms/dm', { chatMate: friend })
+			.then((res) => res.json())
+			.then((data: Dm) => {
+				const dataWithDate = {
+					...data,
+					updatedTime: new Date(data.updatedTime),
+				};
+				setDmList((pre) => ({
+					...pre,
+					dmList: [...pre.dmList, dataWithDate],
+				}));
+				setChat(dataWithDate);
+			})
+			.catch((err) => console.log(err));
+	};
 
 	const onClickFriend = () => {
-		const dm = dmList[0]; //temp
-		setChat(dm);
+		const dm = dmList.find((dm) => dm.chatMate.id === friend.id);
+		if (dm) {
+			setChat(dm);
+			return;
+		}
+		createNewDm();
 	};
 
 	return (
-		<div className={`w-full ${margin}`}>
+		<div className={`w-full ${props.isTheLast ? '' : 'mb-1'}`}>
 			<button
 				className='flex px-3 py-1 w-full hover:bg-gray-200 text-left'
 				onClick={onClickFriend}
