9acbf1ed6ca4b40d1460d1d772617d3a422523b2
soong-e <yujelee@student.42seoul.kr>
2023-10-06 16:14:42 +0900

    feat: 채팅 입장할 때 비밀번호 입력할 수 있게 함

--- src/pages/chat/components/modal/search-channel-item.tsx
+++ src/pages/chat/components/modal/search-channel-item.tsx
@@ -1,8 +1,8 @@
 import { ChatRoomType } from 'ts/enums/chat-room-type.enum';
 import { ChatRoom } from 'ts/interfaces/chat-room.model';
 import { BiCheck, BiHash, BiLock } from 'react-icons/bi';
-import { ListGroup } from 'flowbite-react';
-import { useState } from 'react';
+import { ListGroup, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
 import userData from 'ts/mock/user-data';
 import { ChatParticipantRole } from 'ts/enums/chat-participants-role.enum';
 import { BsDot } from 'react-icons/bs';
@@ -13,14 +13,17 @@ import useFetch from 'hooks/use-fetch';
 import { chatModalState } from 'ts/states/chat-modal-state';
 import { ChatModalStatus } from 'ts/enums/chat-modal-status.enum';
 import { ChatParticipantStatus } from 'ts/enums/chat-participants-status.enum';
-import Status from 'pages/chat/components/status';
 
 export const SearchChannelItem = (props: { channel: ChatRoom }) => {
-	const [isHovered, setIsHovered] = useState(false);
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+	const [input, setInput] = useState<string>('');
+	const [inputFail, setInputFail] = useState<boolean>(false);
+	const [enterPassword, setEnterPassword] = useState<boolean>(false);
 	const setChat = useChangeChat();
 	const setChannelList = useSetRecoilState(chatListState);
 	const setModalStatus = useSetRecoilState(chatModalState);
 	const sendApi = useFetch();
+	const inputRef = useRef<HTMLInputElement>(null);
 	const owner = props.channel.participants.find(
 		(participant) => participant.role === ChatParticipantRole.OWNER
 	);
@@ -34,6 +37,12 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 					participant.status === ChatParticipantStatus.LEFT
 				)
 		).length !== 0;
+
+	useEffect(() => {
+		if (inputRef.current && enterPassword) {
+			inputRef.current.focus();
+		}
+	}, [enterPassword]);
 
 	const onMouseEnter = () => {
 		setIsHovered(true);
@@ -54,6 +63,13 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 	};
 
 	const buttonOnChannel = () => {
+		if (enterPassword) {
+			return (
+				<div className='h-8 focus:outline-none text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 text-xs font-medium rounded-lg px-3 py-2'>
+					입력
+				</div>
+			);
+		}
 		if (isUserInChannel) {
 			return (
 				<div className='h-8 border border-gray-200 focus:outline-none text-gray-800 bg-white hover:bg-gray-300 focus:ring-4 focus:ring-cyan-300 text-xs font-medium rounded-lg px-3 py-2'>
@@ -77,15 +93,27 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 		);
 	};
 
+	const changeInput = () => {
+		setInputFail(true);
+
+		setTimeout(() => {
+			setInputFail(false);
+		}, 1000);
+	};
+
 	const onClickItem = () => {
 		if (isUserInChannel) {
 			setChat(props.channel);
 			setModalStatus(ChatModalStatus.CLOSE);
 			return;
 		}
+		if (props.channel.roomType === ChatRoomType.PROTECTED && !enterPassword) {
+			setEnterPassword(true);
+			return;
+		}
+		const password = enterPassword ? input : null;
 		(async () => {
-			//temp 비번있으면 또 해쉬해서 쳐보내야함..
-			await sendApi('POST', `/chat-rooms/${props.channel.id}`)
+			await sendApi('POST', `/chat-rooms/${props.channel.id}`, { password })
 				.then((res) => {
 					if (res.statusText === 'Unauthorized') {
 						throw Error(res.statusText);
@@ -100,14 +128,49 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 						};
 					});
 					setChat(data);
+					setModalStatus(ChatModalStatus.CLOSE);
 				})
 				.catch((err) => {
 					if (err.message === 'Unauthorized') {
 						console.log('비번 틀림');
+						changeInput();
+						setInput('');
 					}
 				});
-			setModalStatus(ChatModalStatus.CLOSE);
 		})();
+	};
+
+	const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInput(e.target.value);
+	};
+
+	const onClickInput = (e: React.MouseEvent<HTMLInputElement>) => {
+		e.stopPropagation();
+		setInputFail(false);
+	};
+
+	const passwordInput = () => {
+		return (
+			<TextInput
+				color={inputFail ? 'failure' : 'gray'}
+				helperText={
+					inputFail ? (
+						<div className='text-xs font-normal text-red-600'>
+							비밀번호가 일치하지 않습니다.
+						</div>
+					) : null
+				}
+				onClick={onClickInput}
+				ref={inputRef}
+				name='password'
+				type='password'
+				className='w-60 mt-2'
+				sizing='sm'
+				placeholder='채널 비밀번호'
+				value={input}
+				onChange={onChangeInput}
+			/>
+		);
 	};
 
 	return (
@@ -121,12 +184,16 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 					<div className='mr-1'>{channelIcon()}</div>
 					{props.channel.roomName}
 				</div>
-				<div className='flex items-center text-xm text-gray-500 font-light'>
-					{isUserInChannel && badge()}
-					{props.channel.currentPeople}명의 멤버
-					<BsDot />
-					채널 소요주 : {owner?.user.intraId}
-				</div>
+				{!enterPassword ? (
+					<div className='flex items-center text-xm text-gray-500 font-light'>
+						{isUserInChannel && badge()}
+						{props.channel.currentPeople}명의 멤버
+						<BsDot />
+						채널 소유주 : {owner?.user.intraId}
+					</div>
+				) : (
+					passwordInput()
+				)}
 			</div>
 			{isHovered && buttonOnChannel()}
 		</ListGroup.Item>
