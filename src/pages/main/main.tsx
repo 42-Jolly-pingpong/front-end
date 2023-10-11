1a3b110856e1a09f7dcb0bf7e7f01d7c4603bccc
minsubro <mmss3265@gmail.com>
2023-10-11 16:02:37 +0900

    feat:main page 입장시 소켓 연결

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from 'react';
 import JoinButton from './components/join-button';
 import JoinIntro from './components/join-intro';
-import { Socket, io } from 'socket.io-client';
+import { socket } from '../../socket/socket';
 
 const Main = () => {
-	
+	const [isConnected, setIsConnected] = useState<boolean>(socket.connected)
+
+	useEffect(() => {
+		if (!isConnected)
+			socket.connect();
+		function onConnect() {
+			setIsConnected(true);
+		}
+		function onDisconnect() {
+			setIsConnected(false)
+		}
+		socket.on('connect', onConnect)
+		socket.on('disconnect', onDisconnect)
+	}, [])
 
 	return (
 		<div className='flex flex-col justify-center items-center text-center mt-72'>
