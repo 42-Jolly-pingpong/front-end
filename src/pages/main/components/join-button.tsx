d325853de3ab3c58ab5e98865265d9612cce973e
minsubro <mmss3265@gmail.com>
2023-10-12 03:56:12 +0900

    feat:게임 매칭버튼 소켓 이벤트 연결

--- src/pages/main/components/join-button.tsx
+++ src/pages/main/components/join-button.tsx
@@ -1,22 +1,34 @@
 import { Button } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import JoinModal from './join-modal';
+import { socket } from '../../../socket/socket';
 
 const JoinButton = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
-
+	useEffect(() => {
+		if (!socket.connected)
+			socket.connect();
+	}, [])
+	function matching() {
+		socket.emit('matching', '');
+		setIsModalOpen(true);
+	}
+	function cancel() {
+		socket.emit('cancel', '');
+		setIsModalOpen(false)
+	}
 	return (
 		<>
 			<Button
 				className='text-white bg-yellow-300 border border-transparent enabled:hover:bg-yellow-300 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-300'
 				size='xl'
-				onClick={() => setIsModalOpen(true)}
+				onClick={matching}
 			>
 				<img src='images/fire.png' alt='fire' />
 				<div className='font-bold mx-3'> Join Game</div>
 				<img src='images/arrow-right.png' alt='arrow' />
 			</Button>
-			<JoinModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+			<JoinModal isOpen={isModalOpen} onClose={cancel} />
 		</>
 	);
 };
