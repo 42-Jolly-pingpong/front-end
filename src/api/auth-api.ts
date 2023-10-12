bcfb635c791c2f46ea78081aaf13de3a0a3d8674
ebcode2021 <ebcode2021@gmail.com>
2023-10-13 02:41:00 +0900

    로그아웃 기능 구현

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -17,8 +17,27 @@ export const getUserByJwt = async (
 
 		if (response.ok) {
 			const user = await response.json();
-			console.log(user);
 			setUserState(user);
+		}
+	} catch (e) {
+		return undefined;
+	}
+};
+
+export const userSignOut = async (setUserState: any): Promise<void> => {
+	try {
+		console.log('sign out 보내기 전');
+		const response = await fetch('http://localhost:3000/auth/signout', {
+			method: 'GET',
+			credentials: 'include',
+			headers: {
+				origin: 'http://localhost:5173',
+			},
+		});
+
+		if (response) {
+			setUserState(null);
+			console.log('response가 성공');
 		}
 	} catch (e) {
 		return undefined;
