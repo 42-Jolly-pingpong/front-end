3d67b49f77c5c4e221d9f163522dac8ca1db2262
ebcode2021 <ebcode2021@gmail.com>
2023-10-15 19:37:52 +0900

    matching modal

--- src/components/modal/game-match-modal.tsx
+++ src/components/modal/game-match-modal.tsx
@@ -1,4 +1,5 @@
 import { Button, Modal, Spinner } from 'flowbite-react';
+import { useEffect, useState } from 'react';
 import GameOptionControl from 'ts/interfaces/game/game-modal-control';
 
 const GameMatchModal: React.FC<GameOptionControl> = ({
@@ -6,15 +7,38 @@ const GameMatchModal: React.FC<GameOptionControl> = ({
 	onClose,
 	onMatch,
 }) => {
+	const [seconds, setSeconds] = useState(10);
+
+	useEffect(() => {
+		if (isOpen && seconds > 0) {
+			const timer = setInterval(() => {
+				setSeconds((prev) => prev - 1);
+			}, 1000);
+		}
+
+		const timer = setTimeout(() => {
+			onClose();
+		}, seconds * 1000);
+		return () => {
+			clearTimeout(timer);
+			clearInterval(seconds);
+		};
+	}, []);
+
 	return (
-		<Modal show={isOpen} onClose={onClose} dismissible>
-			<Modal.Body>
-				<div className='flex flex-col justify-center relative'>
-					<div>대전자 찾는중...</div>
-					<Spinner></Spinner>
-					<div className='absolute top-5 left-0 right-0 bottom-0'>60</div>
-					<Button>취소하기</Button>
+		<Modal size='md' show={isOpen} onClose={onClose} dismissible>
+			<Modal.Body className='flex flex-col items-center text-center relative'>
+				<div className='text-xl font-extrabold mb-6'>대전자 찾는중...</div>
+				<Spinner className='w-20 h-20 fill-yellow-300'></Spinner>
+				<div className='absolute my-8 top-16 left-0 right-0 bottom-0 text-2xl font-bold'>
+					{seconds}
 				</div>
+				<Button
+					className='mt-6 font-bold text-yellow-300 bg-white enabled:hover:bg-white focus:ring-0  dark:focus:ring-yellow-300 border-yellow-300 border-2 w-32'
+					onClick={() => onClose()}
+				>
+					취소하기
+				</Button>
 			</Modal.Body>
 		</Modal>
 	);
