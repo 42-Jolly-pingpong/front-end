475e18ff2757c0daa78824ff39b97f60da9b7a38
soong-e <yujelee@student.42seoul.kr>
2023-10-16 15:23:15 +0900

    feat: 챗룸 업데이트 됐을 때 이벤트 받도록 함

--- src/pages/chat/handle-chat-socket.tsx
+++ src/pages/chat/handle-chat-socket.tsx
@@ -2,11 +2,13 @@ import useChangeChat from 'hooks/use-change-chat';
 import { chatSocket } from 'pages/chat/chat-socket';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import { ChatRoom } from 'ts/interfaces/chat-room.model';
 import { Chat } from 'ts/interfaces/chat.model';
 import { chatState } from 'ts/states/chat-state';
 
 const HandleChatSocket = () => {
 	const [chat, setChat] = useRecoilState(chatState);
+	const setChatRoom = useChangeChat();
 
 	useEffect(() => {}, []);
 
@@ -19,14 +21,23 @@ const HandleChatSocket = () => {
 			chatSocket.off('getNewChat');
 			chatSocket.on('getNewChat', (data: { roomId: number; newChat: Chat }) => {
 				const { roomId, newChat } = data;
+				console.log('het', newChat.content);
 				if (roomId === chat.chatRoom?.id) {
 					setChat((pre) => ({ ...pre, chats: [...pre.chats, newChat] }));
+				}
+			});
+
+			chatSocket.off('updateChatRoom');
+			chatSocket.on('updateChatRoom', (chatroom: ChatRoom) => {
+				if (chatroom.id === chat.chatRoom?.id) {
+					setChatRoom(chatroom, false);
 				}
 			});
 		}
 
 		return () => {
 			chatSocket.off('getNewChat');
+			chatSocket.off('updateChatRoom');
 		};
 	}, [chat.chatRoom]);
 
