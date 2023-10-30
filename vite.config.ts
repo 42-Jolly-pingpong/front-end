c1050cca39cac2e4daf05f85aba8c8ce9a6b9ed3
get6 <ittae.com@gmail.com>
2023-10-30 11:11:21 +0900

    feat: 도커에서 돌아가게 Vite 환경 설정 추가 #144

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig({
 			socket: '/src/socket'
 		},
 	},
+	sever: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, // needed for the Docker Container port mapping to work
+		strictPort: true,
+		port: 5173, // you can replace this port with any port
+	}
 });
