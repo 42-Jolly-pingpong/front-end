464cb86a59a256a5bd53d14863662c6bd0576341
get6 <ittae.com@gmail.com>
2023-11-05 17:34:30 +0900

    feat: 간극 조정 #139

--- src/pages/main/components/join-intro.tsx
+++ src/pages/main/components/join-intro.tsx
@@ -1,10 +1,10 @@
 const JoinIntro = () => {
 	return (
-		<div className='flex flex-col pb-10'>
-			<div className='text-5xl font-bold pb-4 text-gray-900'>
+		<div className='flex flex-col gap-y-6'>
+			<div className='text-6xl font-extrabold text-gray-900'>
 				언제나 우리곁엔 죠리핑퐁
 			</div>
-			<div className='text-l text-gray-500'>
+			<div className='text-xl text-gray-500'>
 				죠리핑퐁의 죠리(Jolly)라는 단어에는 '엄청난, 굉장한, 훌륭한' 이란 의미가
 				담겨 있어요.
 				<br />
