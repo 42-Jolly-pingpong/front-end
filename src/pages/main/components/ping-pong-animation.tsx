677880b0c620c2483aded2cf7aeebaa37f29f597
get6 <ittae.com@gmail.com>
2023-11-05 17:33:51 +0900

    feat: 핑퐁 애니메이션 구현 완료 #139

--- src/pages/main/components/ping-pong-animation.tsx
+++ src/pages/main/components/ping-pong-animation.tsx
@@ -1,5 +1,31 @@
 const PingPongAnimation = () => {
-	return <div></div>;
+	return (
+		<div className='ping-body w-full h-44'>
+			<div className='pingpong'>
+				<div className='table'>
+					<div className='line' />
+					<div className='net-top' />
+					<div className='net-middle' />
+					<div className='net-bottom' />
+					<div className='net-shadow' />
+				</div>
+				<div className='c1 animate-rotate-c1'>
+					<div className='b1' />
+				</div>
+				<div className='c2 animate-rotate-c2'>
+					<div className='b2' />
+				</div>
+				<div className='c3 animate-rotate-c3'>
+					<div className='b3' />
+				</div>
+				<div className='c4 animate-rotate-c4'>
+					<div className='b4' />
+				</div>
+				<span className='ping animate-anim-ping'>핑</span>
+				<span className='pong animate-anim-pong'>퐁</span>
+			</div>
+		</div>
+	);
 };
 
 export default PingPongAnimation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,22 +26,49 @@ module.exports = {
 			},
 			keyframes: {
 				welcome: {
-					'0%, 100%': {
-						backgroundPosition: 'left top',
-					},
-					'25%': {
-						backgroundPosition: 'right bottom',
-					},
-					'50%': {
-						backgroundPosition: 'left bottom',
-					},
-					'75%': {
-						backgroundPosition: 'right top',
-					},
+					'0%, 100%': { backgroundPosition: 'left top' },
+					'25%': { backgroundPosition: 'right bottom' },
+					'50%': { backgroundPosition: 'left bottom' },
+					'75%': { backgroundPosition: 'right top' },
+				},
+				'ani-ping': {
+					'0%': { opacity: 0.8 },
+					'35%, 100%': { opacity: 0 },
+				},
+
+				rotateC1: {
+					'0%': { opacity: 1, transform: 'rotate(-44deg)' },
+					'35%': { opacity: 1, transform: 'rotate(52deg)' },
+					'36%, 100%': { opacity: 0 },
+				},
+
+				rotateC2: {
+					'0%, 49%': { opacity: 0 },
+					'50%': { opacity: 1, transform: 'rotate(44deg)' },
+					'85%': { opacity: 1, transform: 'rotate(-52deg)' },
+					'86%, 100%': { opacity: 0 },
+				},
+
+				rotateC3: {
+					'0%, 84%': { opacity: 0 },
+					'85%': { opacity: 1, transform: 'rotate(39deg)' },
+					'100%': { opacity: 1, transform: 'rotate(-15deg)' },
+				},
+				rotateC4: {
+					'0%, 34%': { opacity: 0 },
+					'35%': { opacity: 1, transform: 'rotate(-39deg)' },
+					'50%': { opacity: 1, transform: 'rotate(15deg)' },
+					'51%, 100%': { opacity: 0 },
 				},
 			},
 			animation: {
 				welcome: 'welcome 20s ease-in-out infinite',
+				'anim-ping': 'ani-ping 2s linear infinite',
+				'anim-pong': 'ani-ping 2s 1s linear infinite',
+				'rotate-c1': 'rotateC1 2s linear infinite',
+				'rotate-c2': 'rotateC2 2s linear infinite',
+				'rotate-c3': 'rotateC3 2s linear infinite',
+				'rotate-c4': 'rotateC4 2s linear infinite',
 			},
 		},
 	},
