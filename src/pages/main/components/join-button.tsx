cb9f24a1da5f40a883a69a152d9147f556812d9b
get6 <ittae.com@gmail.com>
2023-11-06 12:51:35 +0900

    feat: tailwind는 색만 추가 가능하지 전체 색 적용 이런 건 안되는 듯 #153

--- src/pages/main/components/join-button.tsx
+++ src/pages/main/components/join-button.tsx
@@ -14,6 +14,8 @@ import { GameWaitStatus } from 'ts/enums/game/game-wait.enum';
 import { gameBannerState } from 'ts/states/game/game-banner-state';
 import YellowButton from 'components/button/yellow-button';
 import { userState } from 'ts/states/user-state';
+import { Button } from 'flowbite-react';
+import { HiArrowRight, HiCursorClick } from 'react-icons/hi';
 
 const JoinButton = () => {
 	const [modal, setModal] = useState(false);
@@ -31,10 +33,9 @@ const JoinButton = () => {
 		// 		setGameInfo(newGameInfo);
 		// 	});
 		// }
-
 		// return () => {
 		// 	socket.off('getPlayerInfo');
-		// 	socket.off('gameStart'); 
+		// 	socket.off('gameStart');
 		// };
 	});
 
@@ -53,12 +54,22 @@ const JoinButton = () => {
 
 	return (
 		<>
-			<YellowButton size='xl' onClick={handleButton}>
+			<Button
+				size='lg'
+				onClick={handleButton}
+				// className='bg-yellow-300 enabled:hover:bg-yellow-400'
+			>
+				<HiCursorClick className='mr-2 h-5 w-5' />
+				게임하러 가기
+				<HiArrowRight className='ml-2 h-5 w-5' />
+			</Button>
+			<GameWaitModal show={modal} onClose={handleClose} />
+			{/* <YellowButton size='xl' onClick={handleButton}>
 				<img src='images/fire.png' alt='fire' />
 				<div className='font-bold mx-3'> 게임하러 가기</div>
 				<img src='images/arrow-right.png' alt='arrow' />
 			</YellowButton>
-			<GameWaitModal show={modal} onClose={handleClose} />
+			<GameWaitModal show={modal} onClose={handleClose} /> */}
 		</>
 	);
 };
