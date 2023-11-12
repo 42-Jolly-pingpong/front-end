0c04e285604dc79f537239c4dc3edc729953fd4b
get6 <ittae.com@gmail.com>
2023-11-12 09:34:39 +0900

    fix: placeholder 삭제 #153

--- src/pages/profile/components/modal/item/profile-edit-nickname.tsx
+++ src/pages/profile/components/modal/item/profile-edit-nickname.tsx
@@ -11,13 +11,13 @@ interface NicknameProps {
 const ProfileEditNickname: React.FC<NicknameProps> = ({ onChange }) => {
 	const user = useRecoilValue(userState);
 	const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
-	const [nickname, setNickname] = useState<string>(user!.nickname);
+	const [nickname, setNickname] = useState<string>('');
 	const [errorMessage, setErrorMessage] = useState<string>('');
 
 	const validateNickname = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		onChange('');
 		const nickname = e.target.value;
-		setNickname(nickname);
+		if (nickname === '') setNickname(nickname);
 
 		if (timeoutId) {
 			clearTimeout(timeoutId);
@@ -48,17 +48,16 @@ const ProfileEditNickname: React.FC<NicknameProps> = ({ onChange }) => {
 	return (
 		<div className='flex gap-6'>
 			<div className='font-bold text-sm w-16'>닉네임</div>
-			<TextInput
-				type='search'
-				placeholder={user?.nickname}
-				defaultValue={user?.nickname}
-				// TODO 처음 들어왔을 때 success로 뜨는 문제 해결하기
-				color={errorMessage ? 'failure' : nickname ? 'success' : undefined}
-				onChange={validateNickname}
-				helperText={errorMessage}
-				required
-				className='w-full'
-			/>
+			<div className='flex flex-col w-full'>
+				<TextInput
+					type='search'
+					defaultValue={user?.nickname}
+					color={errorMessage ? 'failure' : nickname ? 'success' : undefined}
+					onChange={validateNickname}
+					helperText={errorMessage}
+					required
+				/>
+			</div>
 		</div>
 	);
 };
