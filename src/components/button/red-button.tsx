e9dfa987e24aaec9eb0c67e3c15081f0ed29e173
get6 <ittae.com@gmail.com>
2023-11-14 19:34:02 +0900

    fix: 버튼 색 수정 #71

--- src/components/button/red-button.tsx
+++ src/components/button/red-button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 const RedButton: React.FC<ButtonProps> = ({ size, children, onClick }) => {
 	return (
 		<Button
-			className='text-white bg-red-700 enabled:hover:bg-red-700 focus:ring-0  border-gray-200 border-2'
+			className='bg-red-700 focus:ring-0 hover:enabled:bg-red-800'
 			size={size}
 			onClick={onClick}
 		>
