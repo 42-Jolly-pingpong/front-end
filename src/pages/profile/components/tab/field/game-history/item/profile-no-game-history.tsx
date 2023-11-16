169d8afa368d968b95e6d9035ab3f89120c1f8c3
ebcode2021 <ebcode2021@gmail.com>
2023-11-16 23:13:07 +0900

    [fix] 본인 프로필일때, tab이 비어있을때 추가 문구

--- src/pages/profile/components/tab/field/game-history/item/profile-no-game-history.tsx
+++ src/pages/profile/components/tab/field/game-history/item/profile-no-game-history.tsx
@@ -1,18 +1,24 @@
+import { useState } from 'react';
 import { BsRocketTakeoff } from 'react-icons/bs';
+import { useRecoilValue } from 'recoil';
+import { ProfileStatus } from 'ts/enums/profile/profile-status.enum';
+import { profileState } from 'ts/states/profile/profile-state';
 
 const ProfileNoGameHistory = () => {
+	const profile = useRecoilValue(profileState);
+	const myHistory = useState(profile.type === ProfileStatus.MINE);
+
 	return (
 		<div className='flex flex-col items-center justify-center gap-6 mt-16'>
 			<div className='w-16 h-16 p-2 rounded-full border-2 border-black justify-center items-center inline-flex'>
 				<BsRocketTakeoff size='40' />
 			</div>
 			<div className='font-extrabold text-3xl'>경기 전적 없음</div>
-			{/*
-			// TODO: 동일 사용자일 때 경기 전적 없음 문구
-			 <div className='font-sm'>
-				누군가와 당신과 함께 경기를하면 전적이 표시됩니다.
-			</div>
-			*/}
+			{myHistory && (
+				<div className='font-sm'>
+					누군가와 당신과 함께 경기를하면 전적이 표시됩니다.
+				</div>
+			)}
 		</div>
 	);
 };
