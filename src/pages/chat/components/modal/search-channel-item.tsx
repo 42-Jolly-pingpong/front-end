2c5752e279a99ae82c1e4417244283105ee6edd3
soong-e <yujelee@student.42seoul.kr>
2023-11-17 11:41:59 +0900

    refactor: 채널 탐색 모달 css수정

--- src/pages/chat/components/modal/search-channel-item.tsx
+++ src/pages/chat/components/modal/search-channel-item.tsx
@@ -18,7 +18,6 @@ import { userState } from 'ts/states/user-state';
 import User from 'ts/interfaces/user.model';
 
 export const SearchChannelItem = (props: { channel: ChatRoom }) => {
-	const [isHovered, setIsHovered] = useState<boolean>(false);
 	const [input, setInput] = useState<string>('');
 	const [inputFail, setInputFail] = useState<boolean>(false);
 	const [enterPassword, setEnterPassword] = useState<boolean>(false);
@@ -49,14 +48,6 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 		}
 	}, [enterPassword]);
 
-	const onMouseEnter = () => {
-		setIsHovered(true);
-	};
-
-	const onMouseLeave = () => {
-		setIsHovered(false);
-	};
-
 	const channelIcon = () => {
 		switch (props.channel.roomType) {
 			case ChatRoomType.PUBLIC:
@@ -68,23 +59,21 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 	};
 
 	const buttonOnChannel = () => {
-		if (enterPassword) {
-			return (
-				<div className='h-8 focus:outline-none text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 text-xs font-medium rounded-lg px-3 py-2'>
-					입력
-				</div>
-			);
-		}
+		const base =
+			'w-[47px] h-8 focus:outline-none focus:ring-4 text-xs font-medium rounded-lg px-3 py-2';
+		const whiteButtonStyle = `${base} border border-gray-200 text-gray-800 bg-white hover:bg-gray-300 focus:ring-cyan-300`;
+		const yellowButtonStyle = `${base} text-white bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 text-xs`;
+
 		if (isUserInChannel) {
 			return (
-				<div className='h-8 border border-gray-200 focus:outline-none text-gray-800 bg-white hover:bg-gray-300 focus:ring-4 focus:ring-cyan-300 text-xs font-medium rounded-lg px-3 py-2'>
-					이동
+				<div onClick={onClickItem} className={whiteButtonStyle}>
+					{'이동'}
 				</div>
 			);
 		}
 		return (
-			<div className='h-8 focus:outline-none text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 text-xs font-medium rounded-lg px-3 py-2'>
-				참여
+			<div onClick={onClickItem} className={yellowButtonStyle}>
+				{enterPassword ? '입력' : '참여'}
 			</div>
 		);
 	};
@@ -172,7 +161,7 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 				ref={inputRef}
 				name='password'
 				type='password'
-				className='w-60 mt-2'
+				className='w-[236px] mt-2'
 				sizing='sm'
 				placeholder='채널 비밀번호'
 				value={input}
@@ -181,29 +170,27 @@ export const SearchChannelItem = (props: { channel: ChatRoom }) => {
 		);
 	};
 
-	return (
-		<ListGroup.Item
-			onMouseEnter={onMouseEnter}
-			onMouseLeave={onMouseLeave}
-			onClick={onClickItem}
-		>
-			<div className='flex flex-col items-start'>
-				<div className='flex items-center text-sm font-bold text-gray-900'>
-					<div className='mr-1'>{channelIcon()}</div>
-					{props.channel.roomName}
-				</div>
-				{!enterPassword ? (
-					<div className='flex items-center text-xm text-gray-500 font-light'>
-						{isUserInChannel && badge()}
-						{props.channel.currentPeople}명의 멤버
-						<BsDot />
-						채널 소유주 : {owner?.user.intraId}
-					</div>
-				) : (
-					passwordInput()
-				)}
+	const channelInfo = () => {
+		return (
+			<div className='flex items-center text-xm text-gray-500 font-light'>
+				{isUserInChannel && badge()}
+				{props.channel.currentPeople}명의 멤버
+				<BsDot />
+				채널 소유주 : {owner?.user.intraId}
 			</div>
-			{isHovered && buttonOnChannel()}
+		);
+	};
+
+	return (
+		<ListGroup.Item>
+			<div className='flex flex-col items-start'>
+				<div className='flex items-start text-sm font-bold text-gray-900'>
+					<div className='mt-1 mr-1'>{channelIcon()}</div>
+					<div className='text-start break-all'>{props.channel.roomName}</div>
+				</div>
+				{!enterPassword ? channelInfo() : passwordInput()}
+			</div>
+			<div>{buttonOnChannel()}</div>
 		</ListGroup.Item>
 	);
 };
