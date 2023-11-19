056f3440f72ecb0e7ebb6ae1d00300111dae8b78
ebcode2021 <ebcode2021@gmail.com>
2023-11-19 14:02:20 +0900

    [fix] profile 접근시 게임히스토리 렌더링 안되는 문제 해결

--- src/pages/profile/components/tab/field/game-history/profile-game-history-list.tsx
+++ src/pages/profile/components/tab/field/game-history/profile-game-history-list.tsx
@@ -1,26 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import { ProfileStatus } from 'ts/enums/profile/profile-status.enum';
 import { GameHistory } from 'ts/interfaces/game-history.model';
 import { profileState } from 'ts/states/profile/profile-state';
+import { ProfileStatus } from 'ts/enums/profile/profile-status.enum';
 import ProfileNoGameHistory from 'pages/profile/components/tab/field/game-history/item/profile-no-game-history';
 import ProfileGameHistoryItem from 'pages/profile/components/tab/field/game-history/item/profile-game-history-item';
-import sendAPI from 'api/sendAPI';
+import { getUserGameHistory } from 'api/user-api';
 
 const ProfileGameHistoryList = () => {
-	// 1. GameHistory model 변경 (erd 설계대로)
-	// 2. 백엔드 GameHistoryDto와 model 비교
-	// 3. src/api/user-api.ts에 백엔드에 이미 있는 '@Post('/:id/history')' 써서 받아오게 함수 만들기
-	const user = useRecoilValue(profileState);
-	const profileType = user.type;
+	const profile = useRecoilValue(profileState);
+	const profileType = profile.type;
 
 	const [historyList, setHistoryList] = useState<GameHistory[]>([]);
 	const fetchHistoryList = async () => {
-		const json = await sendAPI({
-			method: 'GET',
-			url: `/user/${user.user?.id}/history`,
-		});
-		setHistoryList(json);
+		const history: GameHistory[] = await getUserGameHistory(profile.user!.id);
+		setHistoryList(history);
 	};
 
 	useEffect(() => {
@@ -30,7 +24,7 @@ const ProfileGameHistoryList = () => {
 		) {
 			fetchHistoryList();
 		}
-	}, []);
+	}, [profile]);
 
 	if (historyList.length === 0) {
 		return <ProfileNoGameHistory />;
