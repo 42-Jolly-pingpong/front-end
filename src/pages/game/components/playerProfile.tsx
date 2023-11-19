cbb4cb1e359b94bdf32f19f588b38044b5cd377f
ebcode2021 <ebcode2021@gmail.com>
2023-11-19 18:51:27 +0900

    [fix] 아바타 안뜨는 문제

--- src/pages/game/components/playerProfile.tsx
+++ src/pages/game/components/playerProfile.tsx
@@ -1,12 +1,13 @@
-import { useRecoilValue } from "recoil";
-import { gameInfoState } from "ts/states/game/game-info.state";
-import { opponentInfoState } from "ts/states/game/opponent-info-state";
-import { userState } from "ts/states/user-state";
+import { Avatar } from 'flowbite-react';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'ts/states/user-state';
+import { gameInfoState } from 'ts/states/game/game-info.state';
+import { opponentInfoState } from 'ts/states/game/opponent-info-state';
 
 export function PlayerProfile() {
-	const gameInfo = useRecoilValue(gameInfoState)
-	const myInfo = useRecoilValue(userState)
-	const opponentInfo = useRecoilValue(opponentInfoState)
+	const gameInfo = useRecoilValue(gameInfoState);
+	const myInfo = useRecoilValue(userState);
+	const opponentInfo = useRecoilValue(opponentInfoState);
 
 	return (
 		<div className='w-[1152px] flex h-14 justify-center items-center gap-2'>
@@ -14,18 +15,28 @@ export function PlayerProfile() {
 				<div className="text-gray-200 text-lg font-medium font-['Inter'] leading-[27px]">
 					{gameInfo.position == 1 ? myInfo?.nickname : opponentInfo?.nickname}
 				</div>
-				<img
-					className='w-12 h-12 relative rounded-[100px]'
-					src={gameInfo.position == 1 ? (myInfo?.avatarPath || '') : (opponentInfo?.avatarPath || '')}
+				<Avatar
+					size='md'
+					rounded
+					img={
+						gameInfo.position == 1
+							? myInfo?.avatarPath || ''
+							: opponentInfo?.avatarPath || ''
+					}
 				/>
 			</div>
 			<div className="text-gray-200 text-lg font-bold font-['Inter'] leading-normal">
 				vs
 			</div>
 			<div className='h-12 justify-start items-center gap-2 flex'>
-				<img
-					className='w-12 h-12 relative rounded-[100px]'
-					src={gameInfo.position == 2 ? (myInfo?.avatarPath || '') : (opponentInfo?.avatarPath || '')}
+				<Avatar
+					size='md'
+					rounded
+					img={
+						gameInfo.position == 2
+							? myInfo?.avatarPath || ''
+							: opponentInfo?.avatarPath || ''
+					}
 				/>
 				<div className="text-gray-200 text-lg font-medium font-['Inter'] leading-[27px]">
 					{gameInfo.position == 2 ? myInfo?.nickname : opponentInfo?.nickname}
